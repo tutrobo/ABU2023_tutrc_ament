7424438584aeaad80ee698fa9e3303f8bedb865a
Rin Iwai <iwai.rin.ij@tut.jp>
2023-01-23 22:44:45 +0900

    cmake更新

--- tutrc_ament/cmake/tutrc_ament_final.cmake
+++ tutrc_ament/cmake/tutrc_ament_final.cmake
@@ -1,8 +1,3 @@
 macro(tutrc_ament_final)
-  if(BUILD_TESTING)
-    find_package(ament_lint_auto REQUIRED)
-    ament_lint_auto_find_test_dependencies()
-  endif()
-
   ament_auto_package()
 endmacro()
